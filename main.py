93030e02e35e27962629dbf3bf618222a888a6fc
EnriqueC <enriquegcarranco@gmail.com>
2023-11-06 14:55:01 -0700

    sudo coding what I think are the main pieces for the url encoding/decoding

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from flask import Flask
+import shortuuid
 URLShorteningAPI = Flask(__name__)
 
-@URLShorteningAPI.route("/")
-def encode():
+# Creating a dictionary to keep track of the data
+UID_LIST = {}
+
+@URLShorteningAPI.route("/encode")
+def encode(url):
+    # Check to make sure key does not already exists
+    id = shortuuid.ShortUUID().random(length=6)
+    url_short=id
+    original_url=url
+    URL_LIST[id] = original_url
+
     return "<p>encoded url</p>"
     #return encoded(url)
 
-def decode():
+@URLShorteningAPI.route("/decode")
+def decode(shortURL):
+    original_url = UID_LIST[shortURL]
     return "<p>decoded url</p>"
     #return decode(url)
