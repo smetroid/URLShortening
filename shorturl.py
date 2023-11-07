c3ee76ea8fee81ced9b015d55e97c39e2c2e8098
EnriqueC <enriquegcarranco@gmail.com>
2023-11-07 10:16:05 -0700

    adding debugger and virtualenv on the project

--- shorturl.py
+++ shorturl.py
@@ -1,11 +1,11 @@
 from flask import Flask
 import shortuuid
-URLShorteningAPI = Flask(__name__)
+shorturl = Flask(__name__)
 
 # Creating a dictionary to keep track of the data
 UID_LIST = {}
 
-@URLShorteningAPI.route("/encode")
+@shorturl.route("/encode")
 def encode(url):
     # Check to make sure key does not already exists
     id = shortuuid.ShortUUID().random(length=6)
@@ -16,8 +16,11 @@ def encode(url):
     return "<p>encoded url</p>"
     #return encoded(url)
 
-@URLShorteningAPI.route("/decode")
+@shorturl.route("/decode")
 def decode(shortURL):
     original_url = UID_LIST[shortURL]
     return "<p>decoded url</p>"
     #return decode(url)
+
+if __name__ == '__main__':
+    shorturl.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
