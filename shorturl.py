6d5d8879d29ed6cd63518c3423c379c19139ff05
EnriqueC <enriquegcarranco@gmail.com>
2023-11-07 11:08:25 -0700

    working encoding/shortening of url being passed

--- shorturl.py
+++ shorturl.py
@@ -1,26 +1,36 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import shortuuid
 shorturl = Flask(__name__)
 
 # Creating a dictionary to keep track of the data
-UID_LIST = {}
+URL_LIST = {}
 
-@shorturl.route("/encode")
-def encode(url):
-    # Check to make sure key does not already exists
-    id = shortuuid.ShortUUID().random(length=6)
-    url_short=id
-    original_url=url
-    URL_LIST[id] = original_url
+@shorturl.route("/")
+def root():
+    return "<p>this is root</p>"
 
-    return "<p>encoded url</p>"
-    #return encoded(url)
+@shorturl.route("/encode", methods=["POST"])
+def encode():
+    """generate a shortened url
+
+    Returns:
+        json: a serializable json object with a shortkey
+    """
+    try:
+        if request.form['url']:
+            id = shortuuid.ShortUUID().random(length=6)
+            url = request.form['url']
+            URL_LIST[id] = url
+        return jsonify({id:url})
+    except Exception as e:
+        return e
 
 @shorturl.route("/decode")
-def decode(shortURL):
+def decode():
     original_url = UID_LIST[shortURL]
     return "<p>decoded url</p>"
     #return decode(url)
 
+# Needed for VSCode debugger
 if __name__ == '__main__':
     shorturl.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
