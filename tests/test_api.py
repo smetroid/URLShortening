431c5c6912ab65a0bea2a2430008f783f354913e
EnriqueC <enriquegcarranco@gmail.com>
2023-11-08 15:07:28 -0700

    cleaning up test

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,14 +12,20 @@ def client():
     client = shorturl.test_client()
     yield client
     
+def test_encode(client):
+    response = client.get('/encode', data=None, content_type=CONTENT_TYPE)
+    assert b'message' in response.data
+
+
+def test_decode(client):
+    response = client.get('/decode', data=None, content_type=CONTENT_TYPE)
+    assert b'message' in response.data
+
 
 def test_encode_decode(client):
-    assert client.get('/encode', data=None, content_type=CONTENT_TYPE).status_code == 200
     encoded_data = client.post('/encode', data=JSON_DATA, content_type=CONTENT_TYPE)
     id = json.loads(encoded_data.data)['id']
     decode_test = '{"id":"'+id+'"}'
     decoded_data = client.post('/decode', data=decode_test, content_type=CONTENT_TYPE)
     origin_url = json.loads(decoded_data.data)['original_url']
     assert origin_url == DATA['url']
-
-
