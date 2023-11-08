9e115edd94b9953073d8b79f99f1411640bc2295
EnriqueC <enriquegcarranco@gmail.com>
2023-11-08 09:56:15 -0700

    parsing top level domain from url being passed

--- shorturl.py
+++ shorturl.py
@@ -1,14 +1,30 @@
 from flask import Flask, request, jsonify
 import shortuuid
 import json
+from urllib.parse import urlparse
+
+
 shorturl = Flask(__name__)
 
 # Creating a dictionary to keep track of the data
 URL_LIST = {}
 
+
+def get_tld(url):
+    parsed_url = urlparse(url)
+    if parsed_url.netloc:
+        parts = parsed_url.netloc.split('.')
+        if len(parts) >= 2:
+            tld= parts[-1]
+            domain = parts[-2]
+            return f"{domain}.{tld}"
+    return None
+
+
 @shorturl.route("/")
 def root():
     return "<p>this is root</p>"
+
 
 @shorturl.route("/encode", methods=["POST", "GET"])
 def encode():
@@ -20,15 +36,18 @@ def encode():
     try:
         data = request.data
         if type(data) is not str and data is not None:
+            # TODO: need to take the domain from the url being passed
             original_url = json.loads(data)
             id = shortuuid.ShortUUID().random(length=6)
             url = original_url['url']
+            domain = get_tld(url)
+            short_url = domain +"/" + id
             URL_LIST[id] = url
 
-        return jsonify({id:url})
+        return jsonify({"id":id,"short_url": short_url, "original_url":url})
 
     except Exception as e:
-        return jsonify(e.msg)
+        return jsonify(e)
 
 
 @shorturl.route("/decode", methods=["POST"])
