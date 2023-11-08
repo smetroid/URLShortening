1f58aba8b1fd42cf69faa12e08c484b1feec4e1b
EnriqueC <enriquegcarranco@gmail.com>
2023-11-08 13:58:09 -0700

    cleaning up commented code and comments

--- shorturl.py
+++ shorturl.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import shortuuid
 import json
 from urllib.parse import urlparse
+from flask_limiter import Limiter
 
 
 shorturl = Flask(__name__)
@@ -25,34 +26,33 @@ def get_tld(url):
     tld = scheme+"://"+hostname
     return tld
 
-@shorturl.route("/")
-def root():
-    return "<p>this is root</p>"
-
 
 @shorturl.route("/encode", methods=["POST", "GET"])
 def encode():
     """generate a shortened url
 
     Returns:
-        json: a serializable json object with an id, short url, and the original url 
+        json: a json object with an id, short url
     """
     try:
-        data = request.data
-        if type(data) is not str and data is not None and len(data) > 0:
-            original_url = json.loads(data)
+        if request.method == "POST":
+            data = json.loads(request.data)
             id = shortuuid.ShortUUID().random(length=6)
-            url = original_url['url']
+            url = data['url']
             domain = get_tld(url)
             short_url = domain +"/" + id
             URL_LIST[id] = url
-            return jsonify({"id":id,"short_url": short_url, "original_url":url})
-        else:
-            return jsonify({})
+            return jsonify({"id":id,"short_url": short_url})
 
+        if request.method == "GET":
+            return jsonify({"message": "Get request succeeded"})
+
+    # handling the GET request when no data is passed
+    except json.JSONDecodeError as e:
+            return jsonify({"message":"unable to parse request data ... please check documentation"})
 
     except Exception as e:
-        return jsonify(e)
+        return jsonify({"msg": "Exception"})
 
 
 @shorturl.route("/decode", methods=["POST", "GET"])
@@ -64,14 +64,12 @@ def decode():
     """
     try:
         data = request.data
-        # TODO: check for json data
         if type(data) is not str and len(data) > 0:
             shorturl_data = json.loads(data)
             id = shorturl_data['id']
         else:
             return jsonify({})
 
-        #TODO: fix this return, DRY ...
         return jsonify({"id":id,"original_url":URL_LIST[id]})
 
     except KeyError as e:
--- tests/test_api.py
+++ tests/test_api.py
@@ -12,25 +12,14 @@ def client():
     client = shorturl.test_client()
     yield client
     
-#@pytest.fixture()
-#def runner(app):
-#    return app.test_cli_runner()
 
-def test_encode(client):
-    #assert client.get('/encode', data=None, content_type=CONTENT_TYPE).status_code == 200
+def test_encode_decode(client):
+    assert client.get('/encode', data=None, content_type=CONTENT_TYPE).status_code == 200
     encoded_data = client.post('/encode', data=JSON_DATA, content_type=CONTENT_TYPE)
     id = json.loads(encoded_data.data)['id']
-    #assert b'id' in response.data
-    #assert b'short_url' in response.data
-    #assert b'original_url' in response.data
     decode_test = '{"id":"'+id+'"}'
     decoded_data = client.post('/decode', data=decode_test, content_type=CONTENT_TYPE)
     origin_url = json.loads(decoded_data.data)['original_url']
     assert origin_url == DATA['url']
-    #decoded_url = client('/decode', data=DATA, content_type=CONTENT_TYPE)
-    #assert response.headers["Location"] == "/auth/login"
 
-#def test_decode(client):
-#    assert client.get('/decode').status_code == 200
-#    assert client.post('/decode')
 
