608f27df2e59db93325bd44709253b76866345c1
EnriqueC <enriquegcarranco@gmail.com>
2023-11-08 12:07:09 -0700

    working first tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,11 @@
 import pytest
 from shorturl import shorturl
+import json
+
+# test data and defaults
+JSON_DATA = '{ "url": "https://www.yahoo.com/news/dog-adopted-7-years-pennsylvania-210548824.html" }'
+DATA = json.loads(JSON_DATA)
+CONTENT_TYPE ="application/json"
 
 @pytest.fixture()
 def client():
@@ -11,8 +17,20 @@ def client():
 #    return app.test_cli_runner()
 
 def test_encode(client):
-    assert client.get('/encode').status_code == 200
-    data = '{ "url": "https://www.yahoo.com/news/dog-adopted-7-years-pennsylvania-210548824.html" }'
-    assert client.post('/encode', data=data, content_type='application/json').status_code == 200
+    #assert client.get('/encode', data=None, content_type=CONTENT_TYPE).status_code == 200
+    encoded_data = client.post('/encode', data=JSON_DATA, content_type=CONTENT_TYPE)
+    id = json.loads(encoded_data.data)['id']
+    #assert b'id' in response.data
+    #assert b'short_url' in response.data
+    #assert b'original_url' in response.data
+    decode_test = '{"id":"'+id+'"}'
+    decoded_data = client.post('/decode', data=decode_test, content_type=CONTENT_TYPE)
+    origin_url = json.loads(decoded_data.data)['original_url']
+    assert origin_url == DATA['url']
+    #decoded_url = client('/decode', data=DATA, content_type=CONTENT_TYPE)
     #assert response.headers["Location"] == "/auth/login"
+
+#def test_decode(client):
+#    assert client.get('/decode').status_code == 200
+#    assert client.post('/decode')
 
